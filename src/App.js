d354dec56a292dabaaa017ab8a49322b72791935
Caroline Rooney <caroline_rooney@icloud.com>
2021-12-20 02:56:04 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React, {useState} from "react";
-import './App.css';
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
-import Todo from "./components/Todo";
+import React from "react";
+import "./App.css";
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function App() {
-  const [todos, setTodos] = useState([]);
+const Todo = ({ todo, index, markTodo, removeTodo }) => {
+  return (
+    <div
+      className="todo"
+      
+    >
+      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+      <div>
+        <Button className="ongoing" onClick={() => markTodo(index)}>✓</Button>{' '}
+        <Button className="completed" onClick={() => removeTodo(index)}>✕</Button>
+      </div>
+    </div>
+  );
+}
 
-  function addTodo(){
-    setTodos([todo,...todos]);
-  }
+const FormTodo = ({ addTodo }) => {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>To Do</p>
-        <TodoForm addTodo={addTodo} />
-        <TodoList todos={todos} />
-      </header>
+    <Form onSubmit={handleSubmit}> 
+    <Form.Group>
+      <Form.Label><b>Add Todo</b></Form.Label>
+      <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+    </Form.Group>
+    <Button type="submit">
+      Submit
+    </Button>
+  </Form>
+  );
+}
+
+const App = () => {
+  const [todos, setTodos] = React.useState([
+    {
+      text:"Tasks will show like this - now, let's get planning!",
+      isDone:false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">To Do List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                markTodo={markTodo}
+                removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
